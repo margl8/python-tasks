a2accb3ab321e315c326e3193ec6aaa8f9c5f324
Maria Gladisheva <margl8@yandex.ru>
2022-10-11 14:25:16 +0500

    modified:   parser.py

--- parser.py
+++ parser.py
@@ -1,12 +1,30 @@
-#парсер собирает последние 20 постов со стены сообщества
-#и выгружает их в json файл в виде словаря
-#в дальейшем просмотры, лайки и т.д. будут высчитываться прямиком из json-файла
-#но я пока не поняла, как это делать...
+"""
+парсер собирает последние 20 постов со стены сообщества
+и выгружает их в json файл в виде словаря
+в дальейшем просмотры, лайки и т.д. будут высчитываться прямиком из json-файла
+но я пока не поняла, как это делать...
+"""
 
+#импорты
 import requests
+
+#глобальные переменные: аутентификция
 token = "fcf784d2fcf784d2fcf784d206ffe7472fffcf7fcf784d29fdf12d1cff39e144bbbaa42"
 version = 5.131
 domain = "welltex_perm"
+
+#глобальные переменные: расчёт ER 
+likes = int(input('Введите число лайков'))
+comments = int(input('Введите число комментариев'))
+reposts = int(input('Введите число репостов'))
+subscribers = int(input('Введите число подписчиков'))
+views = int(input('Введите число основного охвата'))
+
+"""
+Wall.get
+Stats.getPostReach
+Расчёт ЕР
+"""
 
 response = requests.get ("https://api.vk.com/method/wall.get", 
     params={
@@ -17,3 +35,4 @@ response = requests.get ("https://api.vk.com/method/wall.get",
     )
 data = response.json()
 print(data)
+
