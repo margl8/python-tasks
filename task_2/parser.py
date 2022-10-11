c5fbc9a0f7c48652b7a5c22ff9cd02e4e4d9bf30
Maria Gladysheva <margl8@yandex.ru>
2022-10-11 21:12:39 +0500

    push

--- task_2/parser.py
+++ task_2/parser.py
@@ -1,10 +1,3 @@
-"""
-парсер собирает последние 20 постов со стены сообщества
-и выгружает их в json файл в виде словаря
-в дальейшем просмотры, лайки и т.д. будут высчитываться прямиком из json-файла
-но я пока не поняла, как это делать...
-"""
-
 #импорты
 import requests
 
@@ -12,28 +5,10 @@ import requests
 token = "fcf784d2fcf784d2fcf784d206ffe7472fffcf7fcf784d29fdf12d1cff39e144bbbaa42"
 version = 5.131
 domain = "welltex_perm"
-
-#глобальные переменные: расчёт ER 
-likes = int(input('Введите число лайков'))
-comments = int(input('Введите число комментариев'))
-reposts = int(input('Введите число репостов'))
-subscribers = int(input('Введите число подписчиков'))
-views = int(input('Введите число основного охвата'))
-
-"""
-Wall.get
-Stats.getPostReach
-Расчёт ЕР
-"""
-
 response = requests.get ("https://api.vk.com/method/wall.get", 
-    params={
-        'access_token': token,
-        'v': version,
-        'domain': domain
-    } 
-    )
-data = response.json()
-print(data)
-
-d
+params={
+'access_token': token,
+'v': version,
+'domain': domain
+} 
+)
