90b5452bbd9b6c2e81e3d374dc33e07e31ab816f
Maria Gladysheva <margl8@yandex.ru>
2022-10-11 14:43:38 +0500

    #	renamed:    ER_calculator.ipynb -> task_1/ER_calculator.ipynb #	renamed:    README.md -> task_1/README.md #	renamed:    data_description.ipynb -> task_1/data_description.ipynb #	renamed:    parser.py -> task_1/parser.py

--- task_1/parser.py
+++ task_1/parser.py
@@ -13,19 +13,6 @@ token = "fcf784d2fcf784d2fcf784d206ffe7472fffcf7fcf784d29fdf12d1cff39e144bbbaa42
 version = 5.131
 domain = "welltex_perm"
 
-#глобальные переменные: расчёт ER 
-likes = int(input('Введите число лайков'))
-comments = int(input('Введите число комментариев'))
-reposts = int(input('Введите число репостов'))
-subscribers = int(input('Введите число подписчиков'))
-views = int(input('Введите число основного охвата'))
-
-"""
-Wall.get
-Stats.getPostReach
-Расчёт ЕР
-"""
-
 response = requests.get ("https://api.vk.com/method/wall.get", 
     params={
         'access_token': token,
